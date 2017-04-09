9b69ff573825fa820661dc234640c69c5df76102
EdisonLv <slowwalkerlcr@163.com>
2017-04-09 21:59:54 +0800

    SpringBoot Base

---
+++ SpringBootBase/src/main/java/com/springboot/lvcr/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.springboot.lvcr.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello Spring Boot";
+	}
+
+}
